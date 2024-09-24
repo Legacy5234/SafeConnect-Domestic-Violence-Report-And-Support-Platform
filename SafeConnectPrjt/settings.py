25df533547a4230c92964fb96149c786fec59ccf
TARI SHELL OFIYOU <tarishell@yahoo.com>
2024-09-24 20:38:19 +0100

    update

--- SafeConnectPrjt/settings.py
+++ SafeConnectPrjt/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if ENVIRONMENT == 'production':        
+if ENVIRONMENT == 'development':        
     DEBUG = True
 else:
     DEBUG = False
